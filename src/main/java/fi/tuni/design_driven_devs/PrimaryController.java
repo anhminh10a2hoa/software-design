8fea2288b95301fce03943da82991963ff528358
Minh Hoang <anhminh10a2hoa@gmail.com>
2023-09-29 02:11:49 +0300

    continue to add weather screen

--- src/main/java/fi/tuni/design_driven_devs/PrimaryController.java
+++ src/main/java/fi/tuni/design_driven_devs/PrimaryController.java
@@ -1,12 +1,21 @@
 package fi.tuni.design_driven_devs;
 
-import java.io.IOException;
 import javafx.fxml.FXML;
+import java.net.http.HttpRequest;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
 
 public class PrimaryController {
-
     @FXML
-    private void switchToSecondary() throws IOException {
-        App.setRoot("secondary");
+    private Label humidityLabel;
+    
+    @FXML protected void getWeatherData(ActionEvent event) {
+//        HttpRequest request = HttpRequest.newBuilder()
+//				.uri(URI.create("https://jokes-by-api-ninjas.p.rapidapi.com/v1/jokes"))
+//				.header("X-RapidAPI-Host", "jokes-by-api-ninjas.p.rapidapi.com")
+//				.header("X-RapidAPI-Key", "your-rapidapi-key")
+//				.method("GET", HttpRequest.BodyPublishers.noBody())
+//				.build();
+        humidityLabel.setText("Sign in button pressed");
     }
 }
