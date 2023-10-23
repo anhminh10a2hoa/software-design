fc6122ffdfa33f44805a618c64169b7deec75025
Long Nguyen <long.nguyen@tuni.fi>
2023-10-24 02:44:47 +0300

    Add layout for Forecast

--- src/main/java/fi/tuni/forecast/ForecastController.java
+++ src/main/java/fi/tuni/forecast/ForecastController.java
@@ -1,9 +1,23 @@
 package fi.tuni.forecast;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TitledPane;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
-public class ForecastController {
-
+public class ForecastController implements Initializable {
+//public class ForecastController {
+    private ForecastModel model;
+    @FXML
+    ImageView titleImage;
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        Image forecastIconImage = new Image("https://cdn0.iconfinder.com/data/icons/ikonate/48/line-chart-512.png");
+        titleImage.setImage(forecastIconImage);
+    }
 }
